5c10df5c25d80c303d12f02896c0e19540a50abc
kjdchapman <kjdchapman@gmail.com>
2022-11-02 16:24:59 +0000

    Correct typos (neighbour/neigbour + grid naming in tests)

--- spec/number_of_living_neighbours_spec.rb
+++ spec/number_of_living_neighbours_spec.rb
@@ -3,19 +3,19 @@ require_relative '../view_next_conway_generation'
 describe "#number_of_living_neighbours" do
   it "should tell me there is one live neighbour if one alive in a 2x1 grid" do
     two_by_one_grid = [:empty, :alive]
-    number_of_neighbours = number_of_living_neigbours(two_by_one_grid,row: 0,column: 0)
+    number_of_neighbours = number_of_living_neighbours(two_by_one_grid,row: 0,column: 0)
     expect(number_of_neighbours).to eq 1
   end
 
   it "returns zero if there is no live neighbour to the right" do
     two_by_one_grid = [:empty, :empty]
-    result = number_of_living_neigbours(two_by_one_grid, row: 0, column: 0)
+    result = number_of_living_neighbours(two_by_one_grid, row: 0, column: 0)
     expect(result).to be_zero
   end
 
   it "returns zero if there is no live neighbour to the right or left" do
-    two_by_one_grid = [:empty, :empty, :empty]
-    result = number_of_living_neigbours(two_by_one_grid, row: 0, column: 1)
+    three_by_one_grid = [:empty, :empty, :empty]
+    result = number_of_living_neighbours(three_by_one_grid, row: 0, column: 1)
     expect(result).to be_zero
   end
 end
--- view_next_conway_generation.rb
+++ view_next_conway_generation.rb
@@ -3,7 +3,7 @@ def view_next_conway_generation(generation)
   generation.each do |row|
     future_row = []
     row.each do |cell|
-      living_neighbours = number_of_living_neigbours(generation,row:0,column:0)
+      living_neighbours = number_of_living_neighbours(generation,row:0,column:0)
       future_row << conway_rule(cell,living_neighbours)
     end
     next_generation << future_row
@@ -11,11 +11,11 @@ def view_next_conway_generation(generation)
   next_generation
 end
 
-def conway_rule(cell,number_of_living_neigbours)
+def conway_rule(cell,number_of_living_neighbours)
   :empty
 end
 
-def number_of_living_neigbours(grid,row:,column:)
+def number_of_living_neighbours(grid,row:,column:)
   living_neighbours = 0
 
   if grid != [:empty, :empty]
